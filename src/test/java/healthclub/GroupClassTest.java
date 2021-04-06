4d2f732e998c1777ec03961f11526e95141db479
Pedro Pereira <pedro.l.pereira@tecnico.ulisboa.pt>
2021-04-06 22:02:09 +0100

    Added tests for setBeginHour method from GroupClass class.

--- src/test/java/healthclub/GroupClassTest.java
+++ src/test/java/healthclub/GroupClassTest.java
@@ -14,12 +14,12 @@ public class GroupClassTest {
 	@DataProvider
 	private Object[][] computeValidDataForConstructor() throws InvalidInvocationException {
 		return new Object[][] {
-				{8 * 60, 2, 7, new HealthClub("club1"), 1, true},
-				{22 * 60, 4, 9, new HealthClub("club2"), 3, false},
-				{10 * 60, 1, 12, new HealthClub("club3"), 9, true},
-				{12 * 60, 7, 6, new HealthClub("club4"), 11, false},
-				{14 * 60, 9, 24, new HealthClub("club5"), 15, true},
-				{17 * 60, 12, 15, new HealthClub("club6"), 0, false},
+				{8  * 60, 2 , 7 , new HealthClub("club1"), 1 , true},
+				{22 * 60, 4 , 9 , new HealthClub("club2"), 3 , false},
+				{10 * 60, 1 , 12, new HealthClub("club3"), 9 , true},
+				{12 * 60, 7 , 6 , new HealthClub("club4"), 11, false},
+				{14 * 60, 9 , 24, new HealthClub("club5"), 15, true},
+				{17 * 60, 12, 15, new HealthClub("club6"), 0 , false},
 				{20 * 60, 88, 22, new HealthClub("club7"), 19, false}
 		};
 	}
@@ -42,14 +42,14 @@ public class GroupClassTest {
 	@DataProvider
 	private Object[][] computeInvalidDataForConstructor() throws InvalidInvocationException {
 		return new Object[][] {
-				{8 * 60 - 1, 3, 8, new HealthClub("club1"), 2, true},
-				{22 * 60 + 1, 5, 10, new HealthClub("club2"), 4, false},
-				{9 * 60, 0, 11, new HealthClub("club3"), 7, true},
-				{11 * 60, 6, 5, new HealthClub("club4"), 10, false},
-				{13 * 60, 8, 25, new HealthClub("club5"), 12, true},
-				{15 * 60, 10, 13, null, 17, false},
-				{18 * 60, 50, 20, new HealthClub("club6"), -1, true},
-				{19 * 60, 77, 21, new HealthClub("club7"), 20, true},
+				{8  * 60 - 1, 3 , 8 , new HealthClub("club1"), 2 , true},
+				{22 * 60 + 1, 5 , 10, new HealthClub("club2"), 4 , false},
+				{9  * 60    , 0 , 11, new HealthClub("club3"), 7 , true},
+				{11 * 60    , 6 , 5 , new HealthClub("club4"), 10, false},
+				{13 * 60    , 8 , 25, new HealthClub("club5"), 12, true},
+				{15 * 60    , 10, 13, null                         , 17, false},
+				{18 * 60    , 50, 20, new HealthClub("club6"), -1, true},
+				{19 * 60    , 77, 21, new HealthClub("club7"), 20, true},
 
 		};
 	}
@@ -62,4 +62,32 @@ public class GroupClassTest {
 		// Assert
 	}
 
+	@DataProvider
+	private Object[] computeValidDataForSetBeginHourValid() {
+		return new Object[] {8 * 60, 22 * 60, 12 * 60};
+	}
+
+	@Test(dataProvider = "computeValidDataForSetBeginHourValid")
+	public void testSetBeginHourValid(int beginHour) throws InvalidInvocationException {
+		// Arrange
+		GroupClass groupClass = new GroupClass(8 * 60, 2, 7, new HealthClub("club"), 1, true);
+		// Act
+		groupClass.setBeginHour(beginHour);
+		// Assert
+		assertEquals(groupClass.getBeginHour(), beginHour);
+	}
+
+	@DataProvider
+	private Object[] computeInvalidDataForSetBeginHourValid() {
+		return new Object[] {8 * 60 -1 , 22 * 60 + 1};
+	}
+
+	@Test(dataProvider = "computeInvalidDataForSetBeginHourValid")
+	public void testSetBeginHourInvalid(int beginHour) throws InvalidInvocationException {
+		// Arrange
+		GroupClass groupClass = new GroupClass(8 * 60, 2, 7, new HealthClub("club"), 1, true);
+		// Act
+		assertThrows(InvalidInvocationException.class, () -> {groupClass.setBeginHour(beginHour); });
+		// Assert
+	}
 }
